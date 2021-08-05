1bbd8e416332d375be83b96535074236a583bc03
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-08-05 16:52:03 +0700

    commit pertama

---
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView ;
+    WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView1);
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+//        nnti dicopas aja linik dari web nya dibawah sini yaa :)
+        webView.loadUrl("https://www.kai.id/");
+        //        membuat tampilan full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
